414a79f6bdcf685f40289e895963ea84761f24bb
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-07-02 20:53:17 +0200

    chore: added tags, test: webpage.spec.ts move to test/smoke

--- tests/smoke/webpage.test.ts
+++ tests/smoke/webpage.test.ts
@@ -1,10 +1,10 @@
-import { ArticlesPage } from './src/pages/articles.page';
-import { CommentsPage } from './src/pages/comments.page';
-import { HomePage } from './src/pages/home.page';
+import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentsPage } from '../../src/pages/comments.page';
+import { HomePage } from '../../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify service main pages', () => {
-  test('home page title', async ({ page }) => {
+  test('home page title @GAD-R01-01', async ({ page }) => {
     //Arrange
     const homePage = new HomePage(page);
 
@@ -16,7 +16,7 @@ test.describe('Verify service main pages', () => {
     expect(title).toContain('GAD');
   });
 
-  test('articles page title', async ({ page }) => {
+  test('articles page title @GAD-R01-02', async ({ page }) => {
     // Arrange
     const articlesPage = new ArticlesPage(page);
 
@@ -28,7 +28,7 @@ test.describe('Verify service main pages', () => {
     expect(title).toContain('Articles');
   });
 
-  test('comments page title', async ({ page }) => {
+  test('comments page title @GAD-R01-02', async ({ page }) => {
     // Arrange
     const commentsPage = new CommentsPage(page);
 
