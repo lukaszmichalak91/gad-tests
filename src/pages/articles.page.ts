9e3791445d74e8f1241c55fe413fe85cf30c22d4
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-07-02 22:06:14 +0200

    test: main menu smokes

--- src/pages/articles.page.ts
+++ src/pages/articles.page.ts
@@ -1,8 +1,10 @@
 import { BasePage } from './base.page';
+import { MainMenuComponent } from './components/main-menu.component';
 import { Page } from '@playwright/test';
 
 export class ArticlesPage extends BasePage {
   url = '/articles.html';
+  mainMenu = new MainMenuComponent(this.page);
 
   constructor(page: Page) {
     super(page);
