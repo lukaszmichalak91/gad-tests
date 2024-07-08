618a3274d4ef6716390b29479ddff1206b3d395b
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-07-08 18:58:22 +0200

    test: negative test for register

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -9,6 +9,7 @@ test.describe('Verify register', () => {
   test('register with correct data and login @GAD-R03-01 @GAD-R03-02 @GAD-R03-03', async ({
     page,
   }) => {
+    // Arrange
     const registerUserData: RegisterUser = {
       userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
       userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
@@ -44,5 +45,49 @@ test.describe('Verify register', () => {
     const welcomePage = new WelcomePage(page);
     const titleWelcome = await welcomePage.title();
     expect(titleWelcome).toContain('Welcome');
+  });
+
+  test('not registered with incorrect data and - not valid email @GAD-R03-04', async ({
+    page,
+  }) => {
+    const registerUserData: RegisterUser = {
+      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+      userEmail: '#$%',
+      userPassword: faker.internet.password(),
+    };
+
+    const expectedErrorText = 'Please provide a valid email address';
+    const registerPage = new RegisterPage(page);
+
+    // Act
+    await registerPage.goto();
+    await registerPage.register(registerUserData);
+
+    // Assert
+    await expect(registerPage.emailErrorText).toHaveText(expectedErrorText);
+  });
+
+  test('not registered with incorrect data and - email not provided @GAD-R03-04', async ({
+    page,
+  }) => {
+    // Arrange
+    const registerPage = new RegisterPage(page);
+    await registerPage.goto();
+    await registerPage.userFirstNameInput.fill(
+      faker.person.firstName().replace(/[^A-Za-z]/g, ''),
+    );
+    await registerPage.userLastNameInput.fill(
+      faker.person.lastName().replace(/[^A-Za-z]/g, ''),
+    );
+    await registerPage.passwordInput.fill(faker.internet.password());
+
+    const expectedErrorText = 'This field is required';
+
+    // Act
+    await registerPage.registerButton.click();
+
+    // Assert
+    await expect(registerPage.emailErrorText).toHaveText(expectedErrorText);
   });
 });
