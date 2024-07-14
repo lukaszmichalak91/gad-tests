1af1a1c69f5a36198b5781668c30e1e52a98209d
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-07-14 20:35:58 +0200

    test: second comment test

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -40,7 +40,7 @@ test.describe('Create, verify and delete comment', () => {
     await addArticlesView.createArticle(articleData);
   });
 
-  test('operate on comments @GAD-R05-01', async () => {
+  test('operate on comments @GAD-R05-01 @GAD-R05-02 @GAD-R05-03', async () => {
     const newCommentData = prepareRandomComment();
 
     await test.step('create new comment', async () => {
@@ -97,6 +97,23 @@ test.describe('Create, verify and delete comment', () => {
 
       // Assert
       await expect(updatedArticleComment.body).toHaveText(editCommentData.body);
+    });
+
+    await test.step('create and verify second comment', async () => {
+      // Arrange
+      const secondCommentData = prepareRandomComment();
+
+      // Act
+      await articlePage.addCommentButton.click();
+      await addCommentView.createComment(secondCommentData);
+
+      // Assert
+      const articleComment = articlePage.getArticleComment(
+        secondCommentData.body,
+      );
+      await expect(articleComment.body).toHaveText(secondCommentData.body);
+      await articleComment.link.click();
+      await expect(commentPage.commentBody).toHaveText(secondCommentData.body);
     });
   });
 });
