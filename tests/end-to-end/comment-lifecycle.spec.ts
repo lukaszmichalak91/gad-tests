1bb7292a6f98ca12885a4ac69d093f738cbacb41
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-08-01 19:22:03 +0200

    chore: article fixture

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -1,21 +1,13 @@
-import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { prepareRandomComment } from '@_src/factories/comment.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
-import { AddArticleModel } from '@_src/models/article.model';
 import { AddCommentModel } from '@_src/models/comment.model copy';
-import { ArticlePage } from '@_src/pages/article.page';
 
 test.describe('Create, verify and delete comment @logged', () => {
-  let articleData: AddArticleModel;
-  let articlePage: ArticlePage;
-
-  test.beforeEach(async ({ addArticleView }) => {
-    articleData = prepareRandomArticle();
-    articlePage = await addArticleView.createArticle(articleData);
-  });
-
-  test('operate on comments @GAD-R05-01 @GAD-R05-02 @logged', async () => {
+  test('operate on comments @GAD-R05-01 @GAD-R05-02 @logged', async ({
+    createRandomArticle,
+  }) => {
     const newCommentData = prepareRandomComment();
+    let articlePage = createRandomArticle.articlePage;
 
     await test.step('create new comment', async () => {
       // Arrange
@@ -77,7 +69,10 @@ test.describe('Create, verify and delete comment @logged', () => {
     });
   });
 
-  test('user can add more than one comment to article @GAD-R05-03 @logged', async () => {
+  test('user can add more than one comment to article @GAD-R05-03 @logged', async ({
+    createRandomArticle,
+  }) => {
+    let articlePage = createRandomArticle.articlePage;
     await test.step('create first comment', async () => {
       // Arrange
       const expectedCommentCreatedPopup = 'Comment was created';
