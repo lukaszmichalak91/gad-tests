e87744fbbaeb771a4234c1ea38deda1d30046fde
lukaszmichalak91 <lukaszmichalak1991@gmail.com>
2024-08-05 20:30:52 +0200

    test: api smoke articles extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -4,12 +4,26 @@ test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
   test('GET articles returns status code 200', async ({ request }) => {
     // Arrange
     const expectedStatusCode = 200;
-
     const articlesUrl = 'api/articles';
 
     // Act
     const response = await request.get(articlesUrl);
     // Assert
     expect(response.status()).toBe(expectedStatusCode);
+  });
+
+  test('Get article should return at least one article @predefined_data', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedMinArticleCount = 1;
+    const articlesUrl = 'api/articles';
+
+    // Act
+    const response = await request.get(articlesUrl);
+    const responseJson = await response.json();
+
+    //Assert
+    expect(responseJson.length).toBeGreaterThanOrEqual(expectedMinArticleCount);
   });
 });
